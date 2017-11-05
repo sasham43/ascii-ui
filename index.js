eb814401bbcfdf7f9e644cd7e811bd45fc5cbab7
sasham43 <sashasemail@gmail.com>
2017-11-05 17:46:48 -0600

    connected socket

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static('public'));
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/public/index.html');
 });
+
+io.on('connection', function(socket){
+  // console.log('a user connected');
+  socket.on('listen', function(msg){
+    // io.emit('chat message', msg);
+    console.log('listen')
+  });
+});
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
