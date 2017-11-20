74e57c60740b7d9c00d424d704814087fea9eda8
sasham43 <sashasemail@gmail.com>
2017-11-20 17:35:57 -0600

    port

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var port = process.env.PORT || 3000;
+
 var cassette = require('./cassette.js');
 
 app.use(express.static('public'));
@@ -54,6 +56,6 @@ io.on('connection', function(socket){
 });
 
 
-http.listen(3000, function(){
-  console.log('listening on *:3000');
+http.listen(port, function(){
+  console.log('listening on *:' + port);
 });
